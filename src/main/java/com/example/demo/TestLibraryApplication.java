9d01ac1026d60daf60bd5cc9a4622ca91a01f1a3
ERASTUS-DOH <41060718+ERASTUS-DOH@users.noreply.github.com>
2021-06-25 12:37:54 +0000

    First Commit

---
+++ src/main/java/com/example/demo/TestLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLibraryApplication.class, args);
+	}
+
+}
