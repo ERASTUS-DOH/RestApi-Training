62032cebc7eae64fed0ba0382427246f96ba8fbd
Abdurami Alhaji Taibu <abdurami.taibu@turntabl.io>
2021-06-29 17:57:24 +0000

    Addition of delete request

--- src/main/java/com/example/demo/controllers/LibraryController.java
+++ src/main/java/com/example/demo/controllers/LibraryController.java
@@ -21,7 +21,30 @@ public class LibraryController {
         return fruitLists;
     }
 
+    @DeleteMapping("fruits")
+    public void deleteFruit(@RequestBody String fruits){
+//        String fruit = fruitLists.stream()
+//                .filter(fruitz -> fruitz.equals(fruits))
+//                .findFirst().orElse(null);
+//        if(fruit!=null){
+//            fruitLists.remove(fruit);
+//        }else{
+//            throw new IllegalArgumentException("Fruit doesn't exists");
+//        }
 
+        fruitLists.removeIf(fruit->fruit.equals(fruits));
+//        if(isThere){
+//            fruitLists.remove(fruits);
+//        }else{
+//            throw new IllegalArgumentException("Fruit doesn't exists");
+//        }
+    }
+
+    @PostMapping("fruit2")
+    public String addFruit(@RequestBody String fruit){
+        fruitLists.add(fruit);
+        return fruit;
+    }
 
     @RequestMapping(value="/testEndpoint", method= RequestMethod.GET)
     public String test(){
